d7d85ae1b3973a9e12b7bd607c546eabdd94fbb3
Johan van Soest <johan.vansoest@maastro.nl>
2019-04-14 11:03:45 +0200

    Implemented twitter feed

--- run.py
+++ run.py
@@ -7,4 +7,10 @@ app = Flask('MyDoctor')
 def index():
     return render_template("index_search.html")
 
+@app.route("/physician")
+def physicianPage():
+    return render_template("physician_profile.html",
+                twitter_profile_name="HermMartens",
+                physician_name="Herm Martens")
+
 app.run(debug=True, host='0.0.0.0', port=5000)
